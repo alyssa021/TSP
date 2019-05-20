6152a9aa2df61639de58376debd025d4a3cbb89e
Alyssa Nolan <canalyssa@outlook.com>
2019-05-20 14:24:03 +0530

    Successful Conversion

--- main.pl
+++ main.pl
@@ -1,8 +1,26 @@
-$n_args = $#ARGV;
+use strict;
+use warnings;
+
+my $n_args = $#ARGV;
 
 if($n_args != 0){
     warn "Invaild number of arguments.\n";
     exit 1;
 }
 
-$filename = "$ARGV[0]";
+my $filename = "$ARGV[0]";
+open(my $fh, '<:encoding(UTF-8)', $filename)
+    or die "Could not open file '$filename' $!";
+open(my $fhw, '>', "$filename.temp") or die "Could not open file '$filename' $!";
+
+while(my $row = <$fh>){
+    $row =~ s/\t/    /g;
+    print $fhw $row;
+}
+
+
+close $fhw;
+close $fh;
+my $mv_c = "mv $filename.temp $filename";
+my $exec = `$mv_c`;
+print "done\n";
